6898808d59831d546e012678583a442742e315e1
nicolas <nico.marin@duocuc.cl>
2024-09-02 20:27:31 -0400

    validacion registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage {
 
-  constructor(private router:Router) { }
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) { }
 
-  ngOnInit() {
+  async validarFormulario(form: any) {
+    if (form.valid && form.value.contrasena === form.value.confirmarContrasena) {
+      console.log('Formulario válido:', form.value);
+      // Muestra alerta de éxito
+      const alert = await this.alertController.create({
+        header: 'Éxito',
+        message: 'Registro exitoso. Serás redirigido al inicio de sesión.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+      await alert.onDidDismiss();
+      
+      // Redirige a la página de login
+      this.router.navigate(['/login']);
+    } else {
+      // Muestra alerta de error
+      const alert = await this.alertController.create({
+        header: 'No se ha completado el registro',
+        message: 'No se puede realizar el registro. Por favor, revise los datos ingresados.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+      await alert.onDidDismiss();
+
+      if (form.value.contrasena !== form.value.confirmarContrasena) {
+        console.log('Las contraseñas no coinciden');
+      }
+    }
   }
 
-  irInicio(){
-    this.router.navigate(['/inicio'])
+  irInicio() {
+    this.router.navigate(['/inicio']);
   }
 
-  irZapatillas(){
-    this.router.navigate(['/zapatillas'])
+  irZapatillas() {
+    this.router.navigate(['/zapatillas']);
   }
 
-  irLogin(){
+  irLogin() {
     this.router.navigate(['/login']);
   }
-
 }
